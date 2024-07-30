faf761618073f71abd1f6e7d2b2b9ef4b2694639
George Manov <130544307+Water-Shadow23@users.noreply.github.com>
2024-07-30 18:45:39 +0300

    Added Requester module  for API requests

---
+++ Client/src/services/api/Request/Request.js
@@ -0,0 +1,33 @@
+
+export default class Request{
+    url;
+    options = {
+        method,
+        headers:{
+          'Content-Type': 'application/json',
+        },
+        body
+    }
+    
+    constructor(method,url,body,authKey){
+      this.url = url;
+      this.options.method = method;
+      this.options.body = body || '';
+      if(authKey){
+        this.options.headers['Auth-Key'] = authKey;
+      }
+    }
+    
+   async sendToServer(){
+     try{
+      const res = await fetch(this.url,this.options);
+      const data = await res.json();
+      if(!data.ok){
+       throw data;
+      }
+      return data;  
+    }catch(err){
+      throw err;    
+    }
+   } 
+}
---
+++ Client/src/services/api/Request/RequestMethods.js
@@ -0,0 +1,51 @@
+  import Request from "./Request.js";
+  
+  
+  class GetRequest extends Request{
+  
+    constructor(url,body,authKey){
+      super('GET',url,body,authKey);
+    }
+  }
+  
+  class PostRequest extends Request{
+  
+    constructor(url,body,authKey){
+      super('POST',url,body,authKey);
+    }
+  }
+  
+  class PostRequest extends Request{   
+  
+    constructor(url,body,authKey){
+      super('POST',url,body,authKey);
+    }
+  }
+  
+  class PutRequest extends Request{
+  
+    constructor(url,body,authKey){
+      super('PUT',url,body,authKey);
+    }
+  }
+  
+  class PatchRequest extends Request{
+  
+    constructor(url,body,authKey){
+      super('PATCH',url,body,authKey);
+    }
+  }
+  class DeleteRequest extends Request{
+  
+    constructor(url,body,authKey){
+      super('DELETE',url,body,authKey);
+    }
+  }
+
+export default {
+   GetRequest,
+   PostRequest,
+   PutRequest,
+   PatchRequest,
+   DeleteRequest,  
+}
