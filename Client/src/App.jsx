e314bb8ccd69ddcc6acf0f362f6504fbd2d39e3f
George Manov <130544307+Water-Shadow23@users.noreply.github.com>
2024-08-03 15:47:18 +0300

    Added client-side Router into App component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,21 +1,14 @@
 import './assets/css/style.css'
 import {Routes,Route} from "react-router-dom";
-import ExplorePage from './pages/Explore/ExplorePage.jsx';
-import ProfilePage from './pages/UserProfile/ProfilePage.jsx';
-import HomePage from './pages/Home/HomePage.jsx';
-import EditPage from './pages/EditProfile/EditPage.jsx';
-import LoginPage from './pages/Auth/Login/LoginPage.jsx';
-import RegisterPage from './pages/Auth/Register/RegisterPage.jsx';
-import PreviousProfilePage from './pages/Auth/PreviousProfile/PreviousProfilePage.jsx';
-
+import Router from './routes/Router.jsx';
 
 function App() {
   
   
 
   return (
-    <> 
-    
+    <>
+    <Router />
     </>
   )
 }
