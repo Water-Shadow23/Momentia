ff1b9b35f3bfd1c671fa741313d04f769061312d
George Manov <130544307+Water-Shadow23@users.noreply.github.com>
2024-08-07 08:43:18 +0300

    Implemented client-side create post functionality

--- Client/src/pages/CreatePost/CreatePost.jsx
+++ Client/src/pages/CreatePost/CreatePost.jsx
@@ -1,35 +1,68 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import useImageUpload from "../../hooks/useImageUpload.jsx";
 import PostFileUpload from "./CreateParts/PostFileUpload.jsx";
 import PostViewUpload from "./CreateParts/PostViewUpload.jsx";
+import useErrorBoundary from "../../hooks/UseErrorBoundary.jsx";
+import isBadRequest from "../../utils/errorHandler.js";
+import { errorConstants, overlayConstants } from "../../constants/dispatchConstants.js";
+import usePost from "../../hooks/serviceHooks/usePosts.jsx";
+import { useNavigate } from "react-router-dom";
+import { UseOverlay } from "../../hooks/useOverlay.jsx";
 
 
-export default function CreatePost() {
+export default function CreatePost({overlayDispatch}) {
   const [postData,setPostData] = useState({
-    postImage:'',
-    caption:'', 
+    data:{
+      postImage:'',
+      caption:'', 
+    },
+    isShared:false,
   });
-
   const [isFileOver,setFileOver] = useState(false);
+  const errorDispatch = useErrorBoundary();
+  const navigate = useNavigate();
+
   const [CurrentComponent,setCurrentComponent] = useState(PostFileUpload);
-  
   //
   const [imagePreviewUrl,setImageUploadState,processFile] = useImageUpload()
   
   async function handleImage(e,nextCurrentComponent){
       if(CurrentComponent.name === 'UploadComponent') {
-      const [imageUrl,imageBuffer] = await processFile(e);
+      const [imageUrl] = await processFile(e);
       setFileOver(false); 
       setImageUploadState(setPostData,{
         toProp:'postImage',
         data:{
-          imageUrl:imageUrl,
-          imageBuffer:imageBuffer 
+          imageUrl:imageUrl
         }
       });
       setCurrentComponent(nextCurrentComponent); 
     }
   }
+  
+  
+  const {createPost} = usePost();
+
+  useEffect(()=>{
+   if(postData.isShared){
+     (async function(){
+       try{
+        await createPost(postData.data);
+        overlayDispatch({
+          typeAction:overlayConstants.CLOSE
+        })
+        navigate('/explore'); 
+       }catch(err){
+         if(!isBadRequest(err)){
+           errorDispatch({
+               typeAction:errorConstants.SET_ERROR,
+               error:err
+             });
+         }
+       }
+     })()
+   } 
+  },[postData.isShared])
   
   return (
     <div className="create-sub"
