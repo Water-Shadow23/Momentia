09c4540201c8ebf788d3fb222fdb8010196cc11f
George Manov <130544307+Water-Shadow23@users.noreply.github.com>
2024-08-07 20:56:39 +0300

    Implemented client-side new functionality
    Show profile and other user profiles

--- Client/src/pages/UserProfile/ProfileParts/ProfileData.jsx
+++ Client/src/pages/UserProfile/ProfileParts/ProfileData.jsx
@@ -1,46 +1,87 @@
-import { Link } from "react-router-dom";
+import { useContext, useEffect, useRef, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { AuthContext } from "../../../context/AuthContext";
+import { useUser } from "../../../hooks/serviceHooks/useUser";
 
 export default function ProfileDataHead(){
- 
+   const {authState} = useContext(AuthContext);
+   const [profileData,setProfileData] = useState();
+   const {getData,getUserData} = useUser();
+
+   const params = useParams();
+   let UserIsUs = useRef(!params.hasOwnProperty('userId')); 
+   const userId = params.userId;
+   
+   useEffect(()=>{
+    UserIsUs.current = !params.hasOwnProperty('userId')
+    if(authState.isAuthenticated){
+      (async function(){
+        try{
+         let profileResData;
+         if(UserIsUs.current){
+          profileResData = await getData();
+         }
+         else{
+          profileResData = await getUserData(userId);
+         } 
+         setProfileData(()=>profileResData.data); 
+        }catch(err){
+   
+        } 
+      })() 
+    }
+
+    return ()=>{
+      setProfileData();
+    }
+
+   },[authState,params.userId])
+   
+   
+
    return (
+    <>
+    
     <div className="profile-data-cont">
                     
     <div className="profile-photo-cont">
       <div className="profile-photo">
-        <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/d733c1e1-d7d1-4f92-9abb-628b1aa5af6a/dfzlns7-c3089e5b-f230-4a67-b56a-706f105c6bed.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2Q3MzNjMWUxLWQ3ZDEtNGY5Mi05YWJiLTYyOGIxYWE1YWY2YVwvZGZ6bG5zNy1jMzA4OWU1Yi1mMjMwLTRhNjctYjU2YS03MDZmMTA1YzZiZWQuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.GER60TdONmCscZ3QvsDf0bVQRk9bsnhdB7FB9RviXK0" alt="" />
+        <img src={profileData?.profilePhoto || ''} alt="" />
       </div>
     </div>
     
    <div className="profile-head">
      <div className="profile-actions">
-      <div className="profile-user-name">g_manov_</div>
+      <div className="profile-user-name">{profileData?.username}</div>
 
-       <DisownProfileActions /> 
+       {UserIsUs.current ? 
+       <OwnProfileActions /> 
+       :
+       <DisownProfileActions />
+      }    
 
      </div>
-
+     
      <div className="profile-stats">
-      <div className="count-posts profile-text-medium">0 posts</div>
-      <div className="count-followers profile-text-medium">56 followers</div>
-      <div className="count-following profile-text-medium">142 following</div>
+      <div className="count-posts profile-text-medium">{profileData?.posts} posts</div>
+      <div className="count-followers profile-text-medium">{profileData?.followers.length} followers</div>
+      <div className="count-following profile-text-medium">{profileData?.following.length} following</div>
      </div>
 
      <div className="profile-bio">
-       <span className="bio-user-name">George Manov</span>
-       <div className="bio-user-job">Cat Lover</div> 
+       <span className="bio-user-name">{profileData?.fullName}</span>
+       <div className="bio-user-job">{profileData?.job}</div> 
        <div className="bio-text">
-        Explore Chow Gar Kung Fu at MyChowGar, your premier online school 📚 for martial arts🥋
-        <br />
-        25% off your first order
-        <br />
-        🏷️WELCOM25 coupon👇
+        {profileData?.bio}
       </div> 
-      <a target="_blank" href="https://mychowgar.com/best-ultimate-chow-gar-coaching-program/" className="bio-website">https://mychowgar.com/best-ultimate-chow-gar-coaching-program/</a>
+      <a target="_blank" href={`${profileData?.website}`} className="bio-website">{profileData?.website}</a>
       </div>
 
     </div> 
     
   </div>
+  
+  </>
    ) 
 }
 
@@ -48,9 +89,9 @@ function OwnProfileActions(){
  
     return (
         <>
-        <Link to="/" className="grey-btn follow-btn">Edit profile</Link>   
+        <Link to="/accaunts/edit" className="grey-btn follow-btn">Edit profile</Link>   
 
-        <Link to="/" className="profile-settings">
+        <Link to="/accaunts/edit" className="profile-settings">
           <i className="fa-solid fa-gear profile-head-icon"></i>
         </Link>
         </>
--- Client/src/pages/UserProfile/ProfileParts/ProfilePosts.jsx
+++ Client/src/pages/UserProfile/ProfileParts/ProfilePosts.jsx
@@ -1,101 +1,346 @@
-import { useState } from "react";
-import { Link, Outlet, useLocation } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { Link, Outlet, useOutletContext, useParams } from "react-router-dom";
 import { UseOverlay } from "../../../hooks/useOverlay.jsx";
 import Comments from "../../Comments/Comments.jsx";
 import useTabs from "../../../hooks/useTabs.jsx";
 import { tabs } from "../tabData.jsx";
+import  {useUser}  from "../../../hooks/serviceHooks/useUser.jsx";
+
+import { AuthContext } from "../../../context/AuthContext.jsx";
+import ProfileUserHeadTabs from "../CommonParts/ProfileUserHeadTabs.jsx";
+import { overlayConstants } from "../../../constants/dispatchConstants.js";
+import { NoPosts, NoSaved } from "../NoContent.jsx";
 
 export  function ProfilePostsBody() {
+     
+    const params = useParams();
+    const UserIsUs = !params.hasOwnProperty('userId'); 
+    const userId = params.userId;
+    
 
-    const { overlayDispatch } = UseOverlay();
+    const {authState} = useContext(AuthContext);
+    const [activeTab, setActive] = useTabs(tabs);
+    const [posts,setPosts] = useState();
+    const {getOwnPosts,getSavedPosts,getUserPosts} = useUser();
+
+    useEffect(()=>{
+     if(authState.isAuthenticated){
+       if(UserIsUs) {
+         if(activeTab.key === 'posts' && activeTab.location === '/accaunts'){
+           (async function(){
+             try{
+             const posts =  await getOwnPosts();
+             setPosts(()=>{
+               return posts.data.posts;  
+             });
+             }catch(err){
+   
+             }
+           })(); 
+         }
+         else if(activeTab.key === 'saved' && activeTab.location === '/accaunts/saved'){
+           (async function(){
+             try{
+             const posts = await getSavedPosts();
+             setPosts(()=>{
+                return posts.data.saved;  
+              });
+             }catch(err){
+   
+             }
+           })(); 
+         }
+       }
+       else if(!UserIsUs){
+        (async function(){
+          try{
+          const postsResData =  await getUserPosts(userId);
+          setPosts(()=>{
+            return postsResData.data.posts;  
+          });
+          }catch(err){
+
+          }
+        })(); 
+       }
+     }   
+    },[activeTab,authState])
+
+    function setOuterUserProfileData(){
+        function addOuterComment(postId){
+         setPosts((preData)=>{
+          const post = preData.find((post)=>post._id===postId);
+          post.comments = post.comments + 1;   
+          return [...preData]  
+         });
+        }
+        function addOuterLike(postId,userId){
+         setPosts((preData)=>{
+           const post = preData.find((post)=>post._id===postId);
+           post.likes.push(userId);
+           return [...preData]     
+         });
+        }
+        function removeOuterComment(postId){
+            setPosts((preData)=>{
+                const post = preData.find((post)=>post._id===postId);
+                post.comments = post.comments - 1;
+                return [...preData]     
+              });
+        }
+        function removeOuterLike(postId,userId){
+            setPosts((preData)=>{
+                const post = preData.find((post)=>post._id===postId);
+                post.likes = post.likes.filter(likeId=>likeId!==userId);   
+                return [...preData] 
+               });
+        }
+
+        function removeOuterSave(postId){
+            setPosts((preData)=>{
+                preData = preData.filter(post=>post._id!==postId);   
+                return [...preData]  
+               });
+        }
+       
+       return {
+        addOuterComment,
+        addOuterLike,
+        removeOuterComment,
+        removeOuterLike,
+        removeOuterSave
+       } 
+    } 
 
     return (
+      <>
+      {posts &&
         <div className="profile-posts-cont">
 
             <div className="profile-posts-head">
-                <ProfilePostsHeadTabs />
+             
+                <ProfileUserHeadTabs 
+                UserIsUs={UserIsUs}
+                activeTab={activeTab}
+                setActive={setActive}
+                /> 
             </div>
 
             <div className="profile-posts-body">
-
-               <Outlet />
-
+             
+               <Outlet 
+               context={{
+                   postsData:posts,
+                   setOuterUserProfileData:setOuterUserProfileData()
+                }}
+                />
             </div>
 
         </div>
+              }
+      </>
     )
 }
 
 export function ProfileSavedPosts() {
+    const {postsData,setOuterUserProfileData} = useOutletContext();
+    const {overlayDispatch} = UseOverlay();
+    let [postCont,setPostCont] = useState([]); 
+    useEffect(()=>{
+        
+        if(postCont.length){
+            postCont = [];
+        }
+
+        if(postsData?.length){
+            
+    
+          for(let i = 0 ; i<postsData.length;i++){
+           if(i%3 === 0){
+            postCont.push(
+            <ProfilePostRow key={i}>
+              <ProfilePost
+              overlayDispatch={overlayDispatch}
+              data={postsData[i]}
+              key={postsData[i].id} 
+              setOuterUserProfileData={setOuterUserProfileData}
+              />
+            </ProfilePostRow>
+            )
+           }else{
+            const lastElement = postCont[postCont.length-1];
+            const newChildren = React.Children.toArray(lastElement.props.children);
+            newChildren.push(
+             <ProfilePost 
+             data={postsData[i]} 
+             overlayDispatch={overlayDispatch}
+             setOuterUserProfileData={setOuterUserProfileData}
+              key={postsData[i].id}
+             />
+            )
+    
+            postCont[postCont.length-1] = React.cloneElement(
+              lastElement,
+              {key:lastElement.key},
+              ...newChildren
+            )
+      
+           }
+          } 
+    
+        }
+    
+        setPostCont(()=>[...postCont]);
+    
+       },[postsData]) 
 
     return (
         <>
-            <ProfilePostRow>
-                <ProfilePost id='d' />
-                <ProfilePost id='s' />
-                <ProfilePost id='a' />
-            </ProfilePostRow>
-            <ProfilePostRow>
-                <ProfilePost id=';' />
-                <ProfilePost id='\' />
-                <ProfilePost id=']' />
-            </ProfilePostRow>
-            <ProfilePostRow>
-                <ProfilePost id='p' />
-                <ProfilePost id='8' />
-                <ProfilePost id='7' />
-            </ProfilePostRow>
-
+         {postsData ? 
+          postCont.length ? 
+          postCont 
+          :
+          <NoSaved /> 
+        
+        :
+        '' 
+        }
         </>
     )
 }
 
 
 export function ProfileOwnPosts() {
+    const {postsData,setOuterUserProfileData} = useOutletContext();
+    const {overlayDispatch} = UseOverlay();
+    delete setOuterUserProfileData.removeOuterSave ;
+    let [postCont,setPostCont] = useState([]); 
+    useEffect(()=>{
+        
+        if(postCont.length){
+            postCont = [];
+        }
+        if(postsData?.length){
+            
+    
+          for(let i = 0 ; i<postsData.length;i++){
+           if(i%3 === 0){
+            postCont.push(
+            <ProfilePostRow key={i}>
+              <ProfilePost
+              overlayDispatch={overlayDispatch}
+              data={postsData[i]}
+              key={postsData[i].id} 
+              setOuterUserProfileData={setOuterUserProfileData}
+              />
+            </ProfilePostRow>
+            )
+           }else{
+            const lastElement = postCont[postCont.length-1];
+            const newChildren = React.Children.toArray(lastElement.props.children);
+            newChildren.push(
+             <ProfilePost 
+             data={postsData[i]} 
+             overlayDispatch={overlayDispatch}
+             setOuterUserProfileData={setOuterUserProfileData}
+              key={postsData[i].id}
+             />
+            )
+    
+            postCont[postCont.length-1] = React.cloneElement(
+              lastElement,
+              {key:lastElement.key},
+              ...newChildren
+            )
+      
+           }
+          } 
+    
+        }
+    
+        setPostCont(()=>[...postCont]);
+    
+       },[postsData])
+
     return (
         <>
-            <ProfilePostRow>
-                <ProfilePost id='1' />
-                <ProfilePost id='2' />
-                <ProfilePost id='3' />
-            </ProfilePostRow>
-            <ProfilePostRow>
-                <ProfilePost id='g' />
-                <ProfilePost id='c' />
-                <ProfilePost id='b' />
-            </ProfilePostRow>
-            <ProfilePostRow>
-                <ProfilePost id='a' />
-                <ProfilePost id='s' />
-                <ProfilePost id='d' />
-            </ProfilePostRow>
-
+        {postsData ? 
+          postCont.length ? 
+          postCont 
+          :
+          <NoPosts /> 
+        
+        :
+        '' 
+        }
         </>
     )
 }
 
-
-function ProfilePostsHeadTabs() {
-
-
-    const location = useLocation();
-    const [activeTab, setActive] = useTabs(tabs);
-
+export function UserPosts(){
+  const {postsData,setOuterUserProfileData} = useOutletContext();
+    const {overlayDispatch} = UseOverlay();
+    delete setOuterUserProfileData.removeOuterSave ;
+    let [postCont,setPostCont] = useState([]); 
+    useEffect(()=>{
+        
+        if(postCont.length){
+            postCont = [];
+        }
+        if(postsData?.length){
+            
+    
+          for(let i = 0 ; i<postsData.length;i++){
+           if(i%3 === 0){
+            postCont.push(
+            <ProfilePostRow key={i}>
+              <ProfilePost
+              overlayDispatch={overlayDispatch}
+              data={postsData[i]}
+              key={postsData[i].id} 
+              setOuterUserProfileData={setOuterUserProfileData}
+              />
+            </ProfilePostRow>
+            )
+           }else{
+            const lastElement = postCont[postCont.length-1];
+            const newChildren = React.Children.toArray(lastElement.props.children);
+            newChildren.push(
+             <ProfilePost 
+             data={postsData[i]} 
+             overlayDispatch={overlayDispatch}
+             setOuterUserProfileData={setOuterUserProfileData}
+              key={postsData[i].id}
+             />
+            )
+    
+            postCont[postCont.length-1] = React.cloneElement(
+              lastElement,
+              {key:lastElement.key},
+              ...newChildren
+            )
+      
+           }
+          } 
+    
+        }
+    
+        setPostCont(()=>[...postCont]);
+    
+       },[postsData])
 
     return (
         <>
-            {tabs.map(tab =>
-                <Link to={tab.location} key={tab.key} className={tab.className(activeTab.key)}
-                    onClick={() => {
-                        setActive(tab.key, location.pathname)
-                    }}
-                >
-                    {tab.children}
-                </Link>
-            )}
+        {postsData ? 
+          postCont.length ? 
+          postCont 
+          :
+          '' 
+        :
+        '' 
+        }
         </>
     )
-
 }
 
 function ProfilePostRow({ children }) {
@@ -107,48 +352,34 @@ function ProfilePostRow({ children }) {
     )
 }
 
-function ProfilePost({ id }) {
+function ProfilePost({ data , setOuterUserProfileData , overlayDispatch }) {
 
     return (
-        <Link to="/" className="profile-box">
+        <div  className="profile-box">
             <div className="box-in"
-            //onClick={}
+            onClick={()=>{
+
+              overlayDispatch({
+                typeAction:overlayConstants.OPEN,
+                component:Comments(data._id,setOuterUserProfileData,overlayDispatch),
+                typeOverlay:'Modal' 
+              })  
+            }}
             >
-                <img src={`https://picsum.photos/1000/1000?${id}`} alt="" />
+                <img src={data.postImage} alt="" />
 
                 <div className="profile-post-stats">
                     <div className="profile-post-likes post-stat">
                         <i className="fa-regular fa-heart"></i>
-                        15.4k
+                        {data.likes.length}
                     </div>
                     <div className="profile-post-comments post-stat">
                         <i className="fa-regular fa-comment"></i>
-                        65
+                        {data.comments}
                     </div>
                 </div>
             </div>
-        </Link>
-    )
-}
-
-function NoPosts() {
-
-    return (
-        <div className="noPosts">
-            <h2 className="profile-text-big">Share Photos</h2>
-            <p className="profile-text-small">When you share photos, they will appear on your profile.</p>
-            <Link to='/' className="blue profile-text-medium">Share photos</Link>
         </div>
     )
 }
 
-function NoSaved() {
-
-    return (
-        <div className="noSaved">
-            <h2 className="profile-text-big">Save</h2>
-            <p className="profile-text-small">Save photos that you want to see again. No one can see what you've saved.</p>
-            <Link to='/' className="blue profile-text-medium">Go to save some photos even now.</Link>
-        </div>
-    )
-}
