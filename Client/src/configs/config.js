c7e8ec46840058de51de2b6927931837f402e626
George Manov <130544307+Water-Shadow23@users.noreply.github.com>
2024-08-08 00:11:13 +0300

    Updated .gitignore files to show temporary .env variables

---
+++ Client/src/configs/config.js
@@ -0,0 +1,8 @@
+
+
+export const env = {
+    API_BASE_URL:"http://localhost:3030/api/v1",
+    API_BASE_USER_URL:"/accaunts",
+    API_BASE_RECORD_URL:"/posts",
+    
+}
---
+++ Client/src/configs/envConfig.js
@@ -0,0 +1,9 @@
+// import dotenv from 'dotenv'
+//refference error - proccess is not defined...
+
+import { env } from "./config.js";
+
+export default function config(){
+   
+  return env;
+}
