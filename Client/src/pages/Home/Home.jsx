9beff85cfed049e8deea8befb3184e4a76b9b1db
George Manov <130544307+Water-Shadow23@users.noreply.github.com>
2024-08-08 04:01:11 +0300

    Added client-side auth btns

--- Client/src/pages/Home/Home.jsx
+++ Client/src/pages/Home/Home.jsx
@@ -1,30 +1,39 @@
 import { useContext } from "react";
 import HomePosts from "./HomePosts.jsx";
 import { AuthContext } from "../../context/AuthContext.jsx";
+import { Link } from "react-router-dom";
 
 
 export default function Home() {
-    
-    const {authState} = useContext(AuthContext);
 
-    return (
-        <section className="home">
+   const { authState } = useContext(AuthContext);
+
+   return (
+      <>
+         <section className="home">
             <div className="home-sub home-content">
-                <div className="variant-cont">
-                    <div className="variant">
-                        {authState.isAuthenticated 
+               <div className="variant-cont">
+                  <div className="variant">
+                     {authState.isAuthenticated
                         ?
                         <p>Following</p>
                         :
                         <p>For you</p>
-                        }
-                    </div>
-                </div>
-               
-              <HomePosts />
+                     }
+                  </div>
+                  {!authState.isAuthenticated && 
+                  <div className="auth-btns">
+                        <Link to='/login' className="auth-btn aqua-btn blue">Login</Link>
+                        <Link to='/register' className="auth-btn blue">Register</Link>
+                  </div>
+                  }
+               </div>
+
+               <HomePosts />
 
             </div>
-        </section>
-    )
+         </section>
+      </>
+   )
 }
 
