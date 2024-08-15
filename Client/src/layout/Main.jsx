efda7649cbbd81b588cf51aed424fca62ee9fcdf
George Manov <130544307+Water-Shadow23@users.noreply.github.com>
2024-08-15 17:24:19 +0300

    Updated client-side Main.jsx

--- Client/src/layout/Main.jsx
+++ Client/src/layout/Main.jsx
@@ -2,13 +2,16 @@ import { Outlet } from "react-router-dom";
 import Header from "./Header/Header.jsx";
 
 
-export default function Main(){
+export default function Main({children}){
  
    return (
     <>
       <Header />
       
+      {children ?
+      children :
       <Outlet />
+      }
     </>
    ) 
 }
